6dec8cc9156512c8f458da720317a3203a3ae361
najla <najlachamseddine@yahoo.fr>
2023-11-29 19:28:50 +0000

    server tonic

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,13 @@
+pub mod zkp_auth {
+    include!("./zkp_auth.rs");
+}
+
+use num_bigint::{BigInt, BigUint};
+use zkp_auth::auth_client;
+
 fn main() {
     println!("Client");
+    // register
+    // challenge request
+    // verify request
 }
--- src/server.rs
+++ src/server.rs
@@ -1,26 +1,36 @@
 pub mod zkp_auth {
     include!("./zkp_auth.rs");
 }
+
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use std::collections::HashMap;
+use std::ops::Deref;
 use std::sync::Mutex;
-use tonic::Response;
+use tonic::{Code, Response, Status, transport::Server};
+use zkp_protocol_ex::chaum_pedersen::*;
 
 use num_bigint::BigUint;
-use zkp_auth::{auth_server::Auth, RegisterRequest, RegisterResponse, AuthenticationChallengeRequest, AuthenticationChallengeResponse, AuthenticationAnswerRequest, AuthenticationAnswerResponse};
+use zkp_auth::{
+    auth_server::{Auth, AuthServer}, AuthenticationAnswerRequest, AuthenticationAnswerResponse,
+    AuthenticationChallengeRequest, AuthenticationChallengeResponse, RegisterRequest,
+    RegisterResponse,
+};
 
+#[derive(Default)]
 pub struct AuthUser {
     user_info_map: Mutex<HashMap<String, UserData>>,
-    auth_id_map: Mutex<HashMap<String, UserData>>,
+    auth_id_map: Mutex<HashMap<String, String>>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Clone, Debug, Default)]
 pub struct UserData {
     user: String,
     y1: BigUint,
     y2: BigUint,
     r1: BigUint,
     r2: BigUint,
-    k: BigUint,
+    // k: BigUint,
     c: BigUint,
     s: BigUint,
     session_id: String,
@@ -32,16 +42,16 @@ impl Auth for AuthUser {
         &self,
         request: tonic::Request<RegisterRequest>,
     ) -> std::result::Result<tonic::Response<RegisterResponse>, tonic::Status> {
-        let req = request.into_inner();
-        let user = req.user;
-        let userData = UserData {
-            user,
-            y1: BigUint::from_bytes_be(&req.y1),
-            y2: BigUint::from_bytes_be(&req.y2),
+        let req_data = request.into_inner();
+        let user = req_data.user;
+        let user_data = UserData {
+            user: user.clone(),
+            y1: BigUint::from_bytes_be(&req_data.y1),
+            y2: BigUint::from_bytes_be(&req_data.y2),
             ..Default::default()
         };
         let user_info_map = &mut self.user_info_map.lock().unwrap();
-        user_info_map.insert(user, userData);
+        user_info_map.insert(user, user_data);
 
         Ok(Response::new(RegisterResponse {}))
     }
@@ -49,21 +59,96 @@ impl Auth for AuthUser {
     async fn create_authentication_challenge(
         &self,
         request: tonic::Request<AuthenticationChallengeRequest>,
-    ) -> std::result::Result<
-        tonic::Response<AuthenticationChallengeResponse>,
-        tonic::Status,
-    > {}
+    ) -> std::result::Result<tonic::Response<AuthenticationChallengeResponse>, tonic::Status> {
+        let req_data = request.into_inner();
+        let user = req_data.user;
+        let user_info_map = &mut self.user_info_map.lock().unwrap();
+        if let Some(user_data) = user_info_map.get_mut(&user) {
+            let protocol = get_fixed_zkp_params();
+            let challenge = generate_random_value(&protocol.q);
+            user_data.r1 = BigUint::from_bytes_be(&req_data.r1);
+            user_data.r2 = BigUint::from_bytes_be(&req_data.r2);
+            user_data.c = challenge.clone();
+            let auth_id = create_random_string();
+            let auth_id_map = &mut self.auth_id_map.lock().unwrap();
+            auth_id_map.insert(auth_id.clone(), user);
+            Ok(Response::new(AuthenticationChallengeResponse {
+                auth_id,
+                c: challenge.to_bytes_be(),
+            }))
+        } else {
+            Err(Status::new(
+                Code::NotFound,
+                format!("User: {} not found in database", user),
+            ))
+        }
+    }
 
     async fn verify_authentication(
         &self,
         request: tonic::Request<AuthenticationAnswerRequest>,
-    ) -> std::result::Result<
-        tonic::Response<AuthenticationAnswerResponse>,
-        tonic::Status,
-    >{}
+    ) -> std::result::Result<tonic::Response<AuthenticationAnswerResponse>, tonic::Status> {
+        let req = request.into_inner();
+        let s = req.s;
+        let auth_id = req.auth_id;
+        let auth_id_map = self.auth_id_map.lock().unwrap();
+        if let Some(u) = auth_id_map.get(&auth_id) {
+            let user_info_map = &mut self.user_info_map.lock().unwrap();
+            if let Some(user_data) = user_info_map.get_mut(u) {
+                let protocol = get_fixed_zkp_params();
+                let verified = protocol.verify_solution(
+                    &user_data.c,
+                    &BigUint::from_bytes_be(&s),
+                    &user_data.r1,
+                    &user_data.r2,
+                    &user_data.y1,
+                    &user_data.y2,
+                );
+                if !verified {
+                    return Err(Status::new(
+                        Code::NotFound,
+                        format!("User: {} not found in database", u),
+                    ));
+                }
+                let session_id = create_random_string();
+                user_data.session_id = session_id.clone();
+                Ok(Response::new(AuthenticationAnswerResponse { session_id }))
+            } else {
+                Err(Status::new(
+                    Code::NotFound,
+                    format!("User with a: {} not found in database", u),
+                ))
+            }
+        } else {
+            Err(Status::new(
+                Code::Unauthenticated,
+                format!("Authentication ID: {} not found in database", auth_id),
+            ))
+        }
+    }
+}
+
+pub fn create_random_string() -> String {
+    let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+    rand_string
+}
+
+impl Deref for UserData {
+    type Target = UserData;
+    fn deref(&self) -> &Self::Target {
+        self
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    println!("Server UP");
+    println!("Auth Server");
+    let address = "127.0.0.1:8080".parse().unwrap();
+    let auth_user = AuthUser::default();
+    Server::builder().add_service(AuthServer::new(auth_user)).serve(address).await.unwrap();
+
 }
